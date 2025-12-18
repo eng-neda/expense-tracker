4251521c121a5e12ec049a68ad7feaad21c869e6
eng.neda <eng.pourandish@gmail.com>
2025-12-18 22:55:17 +0330

    chore: initial project setup

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import TransactionTable from './components/TransactionTable'
+function App() {
+  return (
+    <div>
+      <TransactionTable />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/TransactionTable.jsx
@@ -0,0 +1,52 @@
+import './TransactionTable.css'
+import { transAction } from '../data/transaction'
+
+function TransactionTable() {
+  return (
+    <div className="table-container">
+      <h2 className="table-title">تراکنش‌ها</h2>
+
+      <table className="transaction-table">
+        <thead>
+          <tr>
+            <th>تاریخ</th>
+            <th>درآمد (تومان) </th>
+            <th>هزینه (تومان)</th>
+            <th>شرح</th>
+          </tr>
+        </thead>
+        <tbody>
+          {transAction.map((item) => {
+            const faDate = item.date.replace(/\d/g, (d) =>
+              String.fromCharCode(d.charCodeAt(0) + 1728)
+            )
+
+            return (
+              <tr key={item.id} className="transaction-row">
+                {/* ستون راست: تاریخ */}
+                <td className="date-col">{faDate}</td>
+
+                {/* ستون چپ: درآمد یا هزینه */}
+                <td className="amount positive">
+                  {item.income > 0
+                    ? `${item.income.toLocaleString('fa-IR')}+`
+                    : ''}
+                </td>
+                <td className="amount negative">
+                  {item.outcome > 0
+                    ? `${item.outcome.toLocaleString('fa-IR')}-`
+                    : ''}
+                </td>
+
+                {/* ستون راست: توضیحات */}
+                <td className="status">{item.description}</td>
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+export default TransactionTable
