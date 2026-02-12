2f13ad16d83d937ddd750add450599467ca4d3d1
eng.neda <eng.pourandish@gmail.com>
2026-02-12 14:28:03 +0330

    fixing

--- src/components/TransactionTable.jsx
+++ src/components/TransactionTable.jsx
@@ -1,6 +1,7 @@
 import './TransactionTable.css'
 import { renderTransactions } from '../helpers/transactionHelpers.jsx'
-import PlusIcon from '../../fonts/plus.svg'
+import PlusIcon from '../../photos/plus.svg'
+import DangerCircle from '../../photos/Danger Circle.png'
 
 function TransactionTable({ transactions, onOpenModal, onDeleteTransaction }) {
   return (
@@ -31,19 +32,7 @@ function TransactionTable({ transactions, onOpenModal, onDeleteTransaction }) {
             <tr className="empty-state">
               <td colSpan="5">
                 <div className="empty-state-content">
-                  <svg
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                    height={20}
-                    width={20}
-                    stroke="#6B7580"
-                    strokeWidth={1.5}
-                  >
-                    <path d="M2 12a10 10 0 1 0 20 0 10 10 0 1 0 -20 0" />
-                    <path d="M12 7v6" strokeLinecap="round" />
-                    <circle cx="12" cy="16" r=".5" fill="#6B7580" />
-                  </svg>
+                  <img src={DangerCircle} alt="" />
                   <div>شما هنوز تراکنشی وارد نکرده‌اید</div>
                 </div>
               </td>
